76279f3c4f0df1e04e630ecce5589bdd452b166e
FischettiMT <maxwell.fischetti@gmail.com>
2023-09-03 10:38:22 -0400

    another

--- bouncing_balls.py
+++ bouncing_balls.py
@@ -1,13 +1,14 @@
 import pygame
 import os
+import random
 import bouncing_balls_class
 
 # clear terminal
 os.system('cls')
 
 # screen size 
-screen_width = 1_024
-screen_height = 768
+screen_width = 1900
+screen_height = 1000
 screen_size = (screen_width, screen_height)
 
 # pygame initialization
@@ -20,39 +21,36 @@ win = pygame.display.set_mode(screen_size)
 fps_clock = pygame.time.Clock()
 fps = 60
 
-num_of_balls = 10
+num_of_balls = 1000
 
-ball1 = bouncing_balls_class.Ball(50,512,384,2,1,-1,win)
-ball2 = bouncing_balls_class.Ball(25,512,384,1,2,"cyan",win)
-ball3 = bouncing_balls_class.Ball(50,256,192,2,1,-1,win)
-ball4 = bouncing_balls_class.Ball(25,375,400,1,2,-1,win)
+# def __init__(self,radius,x_init,y_init,x_speed,y_speed,clr,screen):
+balls = []
 
+for ball in range(num_of_balls):
+    balls.append(bouncing_balls_class.Ball(
+        random.randint(25,100),
+        random.randint(0,screen_width),
+        random.randint(0,screen_height),
+        random.randint(8,12),
+        random.randint(8,12),
+        -1,win))
+    
 
 # start game loop
 run = True
 while run == True:
-    fps_clock.tick(fps) 
-    win.fill("red")-
+    win.fill("yellow")
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            run = False
             
-    ball1.update()
-    ball1.draw()
-    
-    ball2.update()
-    ball2.draw()
-            
-    ball3.update()
-    ball3.draw()
-    
-    ball4.update()
-    ball4.draw()
+    for ball in balls:
+        ball.update()
+        ball.draw()
    
-    
-    
-    
     pygame.display.flip()
 # end game loop
 
